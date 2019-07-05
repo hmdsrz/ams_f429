628e582284972e4a54134f71abf483a631252410
Hamed Shirazi <hmdsrz@live.com>
2019-07-05 23:11:32 +0430

    init

---
+++ Inc/as7265x.h
@@ -0,0 +1,184 @@
+
+// Code partually copied from Sparkfun Arduino library
+
+#ifndef _AS7265X_H
+#define _AS7265X_H
+
+
+#include "stm32f4xx_hal.h"
+#include "main.h"
+
+#define I2C_AS72XX_SLAVE_STATUS_REG 0x00
+#define I2C_AS72XX_SLAVE_WRITE_REG 0x01
+#define I2C_AS72XX_SLAVE_READ_REG 0x02
+#define I2C_AS72XX_SLAVE_TX_VALID 0x02
+#define I2C_AS72XX_SLAVE_RX_VALID 0x01
+
+
+// Virtual Register addresses
+
+#define AS7265X_HW_VERSION_HIGH   0x00
+#define AS7265X_HW_VERSION_LOW    0x01
+
+#define AS7265X_FW_VERSION_HIGH  0x02
+#define AS7265X_FW_VERSION_LOW  0x03
+
+#define AS7265X_CONFIG      0x04
+#define AS7265X_INTERGRATION_TIME 0x05
+#define AS7265X_DEVICE_TEMP     0x06
+#define AS7265X_LED_CONFIG    0x07
+
+//Raw channel registers
+#define AS7265X_R_G_A     0x08
+#define AS7265X_S_H_B     0x0A
+#define AS7265X_T_I_C     0x0C
+#define AS7265X_U_J_D     0x0E
+#define AS7265X_V_K_E     0x10
+#define AS7265X_W_L_F     0x12
+
+//Calibrated channel registers
+#define AS7265X_R_G_A_CAL   0x14
+#define AS7265X_S_H_B_CAL   0x18
+#define AS7265X_T_I_C_CAL   0x1C
+#define AS7265X_U_J_D_CAL   0x20
+#define AS7265X_V_K_E_CAL   0x24
+#define AS7265X_W_L_F_CAL   0x28
+
+#define AS7265X_DEV_SELECT_CONTROL  0x4F
+
+#define AS7265X_COEF_DATA_0   0x50
+#define AS7265X_COEF_DATA_1   0x51
+#define AS7265X_COEF_DATA_2   0x52
+#define AS7265X_COEF_DATA_3   0x53
+#define AS7265X_COEF_DATA_READ    0x54
+#define AS7265X_COEF_DATA_WRITE   0x55
+
+
+//Settings 
+
+#define AS7265X_POLLING_DELAY 5 //Amount of ms to wait between checking for virtual register changes
+
+#define AS72651_NIR     0x00
+#define AS72652_VISIBLE     0x01
+#define AS72653_UV      0x02
+
+#define AS7265x_LED_WHITE	0x00 //White LED is connected to x51
+#define AS7265x_LED_IR	0x01 //IR LED is connected to x52
+#define AS7265x_LED_UV	0x02 //UV LED is connected to x53
+
+#define AS7265X_LED_CURRENT_LIMIT_12_5MA  0b00
+#define AS7265X_LED_CURRENT_LIMIT_25MA    0b01
+#define AS7265X_LED_CURRENT_LIMIT_50MA    0b10
+#define AS7265X_LED_CURRENT_LIMIT_100MA   0b11
+
+#define AS7265X_INDICATOR_CURRENT_LIMIT_1MA   0b00
+#define AS7265X_INDICATOR_CURRENT_LIMIT_2MA   0b01
+#define AS7265X_INDICATOR_CURRENT_LIMIT_4MA   0b10
+#define AS7265X_INDICATOR_CURRENT_LIMIT_8MA   0b11
+
+#define AS7265X_GAIN_1X   0b00
+#define AS7265X_GAIN_37X   0b01
+#define AS7265X_GAIN_16X   0b10
+#define AS7265X_GAIN_64X   0b11
+
+#define AS7265X_MEASUREMENT_MODE_4CHAN   0b00
+#define AS7265X_MEASUREMENT_MODE_4CHAN_2   0b01
+#define AS7265X_MEASUREMENT_MODE_6CHAN_CONTINUOUS   0b10
+#define AS7265X_MEASUREMENT_MODE_6CHAN_ONE_SHOT   0b11
+
+
+
+typedef struct as7265x_named_channels {
+	// AS72651
+	float A;
+	float B;
+	float C;
+	float D;
+	float E;
+	float F;
+
+	// AS72652
+	float G;
+	float H;
+	float I;
+	float J;
+	float K;
+	float L;
+
+	// AS72651
+	float R;
+	float S;
+	float T;
+	float U;
+	float V;
+	float W;
+} as7265x_named_channels_t;
+
+typedef struct as7265x_channels {
+	float channel[18];
+} as7265x_channels_t;
+
+typedef struct as7265x_raw_channels {
+	uint16_t channel[18];
+} as7265x_raw_channels_t;
+
+typedef struct as7265x_wavelengths {
+	int channel[18];
+} as7265x_wavelengths_t;
+
+struct {
+	union {
+		struct as7265x_named_channels named_channels;
+		struct as7265x_channels channels;
+	} data;
+} as7265x_channel_union_t;
+
+
+/**
+ * How to reorder channels so that channels are ordered by ascending wavelength.
+ * The input order is assumed to be the AS72651 channels, followed by AS72652
+ * and finally the AS72653 channels. Ie channels
+ * R,S,T,U,V,W,   G,H,I,J,K,L,  A,B,C,D,E,F.  Output channel order is
+ * A,B,C,D,E,F, G,H, R, I, S, J, T,U,V,W, K,L.
+ */
+static const uint8_t as7265x_channel_order_table[] =  {
+	12,13,14,15,16,17,    6,7,0,8,1,9,2,3,4,5,10,11
+};
+
+/**
+ * Peak sensitivity frequency of the unordered channels (unit nm)
+ */
+static const int as7265x_unordered_channel_wavelength[] = {
+	610, 680, 730, 760, 810, 860, 560, 585, 645, 705, 900, 940, 410, 435, 460, 485, 510, 535
+};
+
+/**
+ * Peak sensitivity frequency of the wavelength ascending ordered channels (unit nm).
+ */
+static const int as7265x_ordered_channel_wavelength[] = {
+	410, 435, 460, 485, 510, 535, 560, 585, 610, 645, 680, 705, 730, 760, 810, 860, 900, 940
+};
+
+int     as7265x_is_data_available(I2C_HandleTypeDef *hi2c);
+void    as7265x_set_gain(I2C_HandleTypeDef *hi2c, int gain);
+void    as7265x_set_integration_time(I2C_HandleTypeDef *hi2c, uint8_t time);
+void    as7265x_set_bulb_current(I2C_HandleTypeDef *hi2c, uint8_t device, uint8_t current);
+void    as7265x_bulb_enable(I2C_HandleTypeDef *hi2c, uint8_t device);
+void    as7265x_bulb_disable(I2C_HandleTypeDef *hi2c, uint8_t device);
+void    as7265x_indicator_enable(I2C_HandleTypeDef *hi2c);
+void    as7265x_indicator_disable(I2C_HandleTypeDef *hi2c);
+void    as7265x_vreg_write(I2C_HandleTypeDef *hi2c, uint8_t vreg, uint8_t value);
+uint8_t as7265x_vreg_read(I2C_HandleTypeDef *hi2c, uint8_t vreg);
+void    as7265x_set_measurement_mode(I2C_HandleTypeDef *hi2c, uint8_t mode);
+void    as7265x_device_select(I2C_HandleTypeDef *hi2c, uint8_t device);
+void    as7265x_soft_reset(I2C_HandleTypeDef *hi2c);
+float   as7265x_get_calibrated_value (I2C_HandleTypeDef *hi2c, uint8_t device, uint8_t base_addr);
+void    as7265x_get_all_calibrated_values (I2C_HandleTypeDef *hi2c, as7265x_channels_t *channels);
+void    as7265x_get_all_raw_values (I2C_HandleTypeDef *hi2c, as7265x_raw_channels_t *channels);
+void    as7265x_order_calibrated_channels(I2C_HandleTypeDef *hi2c, as7265x_channels_t *channels);
+void    as7265x_order_raw_channels(I2C_HandleTypeDef *hi2c, as7265x_raw_channels_t *channels);
+as7265x_wavelengths_t    as7265x_get_unordered_channel_wavelengths();
+as7265x_wavelengths_t    as7265x_get_ordered_channel_wavelengths();
+
+#endif
+
---
+++ Src/as7265x.c
@@ -0,0 +1,383 @@
+/**
+ * AS7265x triad spectroscopic sensor I2C library.
+ * 
+ * See https://ams.com/as7265x for sensor datasheet.
+
+ * Most recent code from:
+ * https://github.com/jdesbonnet/as7265x 
+ */
+
+#include <stdio.h>
+#include <string.h>
+//#include <unistd.h>
+#include <stdlib.h>
+
+//#include "i2c.h"
+#include "as7265x.h"
+
+#include "stm32f4xx_hal.h"
+#include "main.h"
+
+#define AS7265X_I2C_ADDR 0x49
+
+I2C_HandleTypeDef hi2c1;
+//static I2C_HandleTypeDef * I2Cdev_hi2c;
+
+//I2Cdev_hi2c = hi2c;
+/**
+ * Read a I2C (real) register from AS7265x
+ */
+int i2cm_read(I2C_HandleTypeDef *hi2c, int addr) {
+	uint8_t result;
+//	i2c_register_read(&hi2c1, AS7265X_I2C_ADDR, addr, &result);
+//	HAL_I2C_Mem_Read(hi2c, AS7265X_I2C_ADDR, addr, I2C_MEMADD_SIZE_8BIT, &result , 1 , 100); //AS7265X_I2C_ADDR
+	return HAL_I2C_Mem_Read(&hi2c1,(uint16_t) AS7265X_I2C_ADDR ,(uint16_t)  addr, I2C_MEMADD_SIZE_8BIT, &result , 1 , 500);
+//	return (HAL_I2C_Mem_Read(hi2c,(uint16_t) AS7265X_I2C_ADDR ,(uint16_t)  addr, I2C_MEMADD_SIZE_8BIT, &result , 1 , 500) || result);
+// if((HAL_I2C_Mem_Read(hi2c,(uint16_t) AS7265X_I2C_ADDR ,(uint16_t)  addr, I2C_MEMADD_SIZE_8BIT, &result , 1 , 500)) == HAL_ERROR) return -1;
+//
+// return HAL_I2C_Mem_Read(hi2c,(uint16_t) AS7265X_I2C_ADDR ,(uint16_t)  addr, I2C_MEMADD_SIZE_8BIT, &result , 1 , 500);
+//	HAL_I2C_Master_Receive(hi2c,AS7265X_I2C_ADDR,addr,)
+
+
+
+} 
+
+
+/**
+ * Write a I2C (real) register to AS7265x.
+ */
+void i2cm_write(I2C_HandleTypeDef *hi2c, int addr, uint8_t value) {
+//	i2c_register_write(&hi2c1, AS7265X_I2C_ADDR, addr, value);
+	HAL_I2C_Mem_Write(&hi2c1,(uint16_t)AS7265X_I2C_ADDR ,(uint16_t)  addr, I2C_MEMADD_SIZE_8BIT, value, 1 , 500); //AS7265X_I2C_ADDR
+}
+
+/**
+ * Write to AS7265x virtual register. Based on code in the AS7265x datasheet.
+ */
+void as7265x_vreg_write(I2C_HandleTypeDef *hi2c, uint8_t virtualReg, uint8_t d)
+{
+	volatile uint8_t status;
+	while (1)
+	{
+		// Read slave I²C status to see if the write buffer is ready.
+		status = i2cm_read(&hi2c1,I2C_AS72XX_SLAVE_STATUS_REG);
+		if ((status & I2C_AS72XX_SLAVE_TX_VALID) == 0) 
+			// No inbound TX pending at slave. Okay to write now.
+			break ;
+	}
+	// Send the virtual register address (enabling bit 7 to indicate a write).
+	i2cm_write(&hi2c1, I2C_AS72XX_SLAVE_WRITE_REG, (virtualReg | 0x80)) ;
+	while (1)
+	{
+		// Read the slave I²C status to see if the write buffer is ready.
+		status = i2cm_read(&hi2c1, I2C_AS72XX_SLAVE_STATUS_REG) ;
+		if ((status & I2C_AS72XX_SLAVE_TX_VALID) == 0)
+		// No inbound TX pending at slave. Okay to write data now.
+		break ;
+	}
+	// Send the data to complete the operation.
+	i2cm_write(&hi2c1,I2C_AS72XX_SLAVE_WRITE_REG, d) ;
+}
+
+/**
+ * Read from AS7265x virtual register. Based on code in the AS7265x datasheet. 
+ */
+uint8_t as7265x_vreg_read(I2C_HandleTypeDef *hi2c, uint8_t virtualReg)
+{
+	volatile uint8_t status, d;
+
+	status = i2cm_read(&hi2c1, I2C_AS72XX_SLAVE_STATUS_REG);
+	if ( (status & I2C_AS72XX_SLAVE_RX_VALID) != 0)  {
+		// data to be read
+		d = i2cm_read(&hi2c1, I2C_AS72XX_SLAVE_READ_REG);
+	}
+
+	// Wait for WRITE flag to clear
+	while (1)
+	{
+		// Read slave I²C status to see if the read buffer is ready.
+		status = i2cm_read(&hi2c1, I2C_AS72XX_SLAVE_STATUS_REG) ;
+		if ((status & I2C_AS72XX_SLAVE_TX_VALID) == 0)
+		// No inbound TX pending at slave. Okay to write now.
+		break;
+	}
+
+
+	// Send the virtual register address (disabling bit 7 to indicate a read).
+	i2cm_write(&hi2c1, I2C_AS72XX_SLAVE_WRITE_REG, virtualReg);
+
+
+	while (1)
+	{
+		// Read the slave I²C status to see if our read data is available.
+		status = i2cm_read(&hi2c1, I2C_AS72XX_SLAVE_STATUS_REG);
+		if ((status & I2C_AS72XX_SLAVE_RX_VALID)!= 0)
+		// Read data is ready.
+		break;
+	}
+
+	// Read the data to complete the operation.
+	d = i2cm_read(&hi2c1, I2C_AS72XX_SLAVE_READ_REG) ;
+	return d;
+}
+
+/**
+ * Test DATA_RDY flag of configuration virtual register (add
+ *
+ * @return 0 if not set, non-zero if set.
+ */
+int as7265x_is_data_available (I2C_HandleTypeDef *hi2c)
+{
+	int status = as7265x_vreg_read(&hi2c1, AS7265X_CONFIG);
+	return (status & (1<<1) );
+}
+
+/**
+ * Select device
+ *
+ * @param device 0=master; 1=first slave; 2=second slave
+ */
+void as7265x_device_select(I2C_HandleTypeDef *hi2c, uint8_t device) {
+	as7265x_vreg_write(&hi2c1, AS7265X_DEV_SELECT_CONTROL, device);
+}
+
+
+/**
+ * Set amplifier gain
+
+ * @param gain 0= 1x (default), 1=3.7x, 2=16x, 3=64x
+ */
+void as7265x_set_gain (I2C_HandleTypeDef *hi2c, int gain) 
+{
+
+	int value = as7265x_vreg_read(&hi2c1, AS7265X_CONFIG);
+	value &= 0b11001111; // clear gain bits
+	value |= (gain&0b11) << 4;
+	as7265x_vreg_write(&hi2c1, AS7265X_CONFIG,value);
+}
+
+/**
+ * Set ADC integration time. 
+ *
+ * @param time from 1..255. 2.8ms units.
+ */
+void as7265x_set_integration_time(I2C_HandleTypeDef *hi2c, uint8_t time)
+{
+	as7265x_vreg_write(&hi2c1, AS7265X_INTERGRATION_TIME, time);
+}
+
+/**
+ * Set bulb current.
+ *
+ * @param device 0, 1, or 2
+ * @param current 
+ */
+void as7265x_set_bulb_current(I2C_HandleTypeDef *hi2c, uint8_t device, uint8_t current)
+{
+	as7265x_device_select(&hi2c1,device);
+
+	current &= 0b11;
+
+	uint8_t value = as7265x_vreg_read(&hi2c1,AS7265X_LED_CONFIG);
+	value &= 0b11001111; //Clear ICL_DRV bits
+	value |= (current << 4); //Set ICL_DRV bits with user's choice
+	as7265x_vreg_write(&hi2c1,AS7265X_LED_CONFIG, value);
+}
+
+/**
+ * Bulb enable / disable
+ */
+void as7265x_bulb_enable (I2C_HandleTypeDef *hi2c, uint8_t device) 
+{
+	as7265x_device_select(&hi2c1, device);
+
+	uint8_t value = as7265x_vreg_read(&hi2c1, AS7265X_LED_CONFIG);
+	// bit 3: bulb en/disable
+	value |= (1 << 3);
+	as7265x_vreg_write(&hi2c1, AS7265X_LED_CONFIG, value);
+}
+
+void as7265x_bulb_disable (I2C_HandleTypeDef *hi2c, uint8_t device)
+{
+        as7265x_device_select(&hi2c1, device);
+	uint8_t value = as7265x_vreg_read(&hi2c1, AS7265X_LED_CONFIG);
+	// bit 3: bulb en/disable
+	value &= ~(1 << 3);
+	as7265x_vreg_write(&hi2c1, AS7265X_LED_CONFIG, value);
+}
+
+void as7265x_indicator_enable (I2C_HandleTypeDef *hi2c) 
+{
+	as7265x_device_select(&hi2c1, 0);
+	uint8_t value = as7265x_vreg_read(&hi2c1, AS7265X_LED_CONFIG);
+	value |= (1<<0);
+	as7265x_vreg_write(&hi2c1, AS7265X_LED_CONFIG, value);
+}
+
+void as7265x_indicator_disable (I2C_HandleTypeDef *hi2c) 
+{
+        as7265x_device_select(&hi2c1, 0);
+        uint8_t value = as7265x_vreg_read(&hi2c1, AS7265X_LED_CONFIG);
+        value &= ~(1<<0);
+        as7265x_vreg_write(&hi2c1, AS7265X_LED_CONFIG, value);
+}
+
+/**
+ * Set measurement mode.
+ * 
+ * @param mode 2 = all 6 channels continuous; 3 = one shot all channels
+ */
+void as7265x_set_measurement_mode(I2C_HandleTypeDef *hi2c, uint8_t mode) 
+{
+	uint8_t value = as7265x_vreg_read(&hi2c1, AS7265X_CONFIG);
+	value &= 0b11110011;
+	value |= (mode&0b11) << 2;
+	as7265x_vreg_write(&hi2c1, AS7265X_CONFIG, value);
+}
+
+/**
+ * Read calibrated value (IEEE 754 float)
+ */
+float as7265x_get_calibrated_value (I2C_HandleTypeDef *hi2c, uint8_t device, uint8_t base_addr)
+{
+	int i;
+	uint8_t value;
+	uint32_t shift_reg = 0;
+
+	as7265x_device_select(&hi2c1, device);
+
+	for (i = base_addr; i < base_addr+4; i++) {
+		shift_reg <<= 8;
+		value = as7265x_vreg_read(&hi2c1, i);
+		shift_reg |= value;
+	}
+	// convert content of shift_reg to floating point
+	float ret;
+	memcpy (&ret, &shift_reg, sizeof(float));
+	return ret;
+}
+
+/**
+ * Read raw value (16 bit unsigned integer)
+ */
+int as7265x_get_raw_value (I2C_HandleTypeDef *hi2c, uint8_t device, uint8_t base_addr)
+{
+	as7265x_device_select(&hi2c1, device);
+        uint32_t value = (as7265x_vreg_read(&hi2c1, base_addr)<<8);
+	value |= as7265x_vreg_read(&hi2c1,base_addr+1);
+	return value;
+}
+
+
+/**
+ * Read all 18 channels. Channels AS72651 (vis): channels 0-5, AS72652 (vis+IR): channels 6-11,
+ * AS72653 (vis+UV): channels 12-17.
+ */
+void as7265x_get_all_calibrated_values (I2C_HandleTypeDef *hi2c, as7265x_channels_t *channels)
+{
+
+	uint8_t base_addr;
+	int channel_index = 0;
+	uint8_t device;
+	float v;
+
+	for (device = 0; device < 3; device++) {
+		for (base_addr = 0x14; base_addr < 0x2c; base_addr += 4) {	
+			v = as7265x_get_calibrated_value (&hi2c1, device, base_addr);
+			channels->channel[channel_index] = v;
+			channel_index++;
+		}
+	}
+
+}
+/**
+ * Read all 18 channels raw ADC. Channels AS72651 (vis): channels 0-5, AS72652 (vis+IR): channels 6-11,
+ * AS72653 (vis+UV): channels 12-17
+ */
+
+void as7265x_get_all_raw_values (I2C_HandleTypeDef *hi2c, as7265x_raw_channels_t *channels) 
+{
+	int base_addr;
+	int device;
+	int channel_index = 0;
+	for (device = 0; device < 3; device++) {
+		for (base_addr = 0x8; base_addr < 0x14; base_addr += 2) {
+			channels->channel[channel_index] = (uint16_t)as7265x_get_raw_value(&hi2c1, device, base_addr);
+			channel_index++;
+		}
+	}
+
+}
+
+
+/**
+ * Order channels in ascending wavelength.
+ */
+void as7265x_order_calibrated_channels(I2C_HandleTypeDef *hi2c, as7265x_channels_t *channels) 
+{
+	float buf[18];
+	int i;
+	for (i = 0; i < 18; i++) {
+		buf[i] = channels->channel[as7265x_channel_order_table[i]];
+	}
+	for (i = 0; i < 18; i++) {
+		channels->channel[i] = buf[i];
+	}
+}
+
+void as7265x_order_raw_channels(I2C_HandleTypeDef *hi2c, as7265x_raw_channels_t *channels) 
+{
+	uint16_t buf[18];
+	int i;
+	for (i = 0; i < 18; i++) {
+		buf[i] = channels->channel[as7265x_channel_order_table[i]];
+	}
+	for (i = 0; i < 18; i++) {
+		channels->channel[i] = buf[i];
+	}
+}
+
+
+void as7265x_measure(I2C_HandleTypeDef *hi2c)
+{
+	int i;
+	as7265x_set_measurement_mode(&hi2c1, AS7265X_MEASUREMENT_MODE_6CHAN_ONE_SHOT);
+	for (i = 0; i < 100; i++) {
+		if ( as7265x_is_data_available(&hi2c1) ) 
+		{
+			break;
+		}
+	}
+
+
+	as7265x_get_calibrated_value (&hi2c1, AS7265X_R_G_A_CAL, AS72653_UV);
+
+}
+
+void as7265x_soft_reset (I2C_HandleTypeDef *hi2c) 
+{
+	as7265x_vreg_write(&hi2c1, AS7265X_CONFIG, (1<<7));
+}
+
+
+as7265x_wavelengths_t as7265x_get_unordered_channel_wavelengths (void)
+{
+	int i = 0;
+	as7265x_wavelengths_t ret;
+	for (i = 0; i < 18; i++) {
+		ret.channel[i] = as7265x_unordered_channel_wavelength[i];
+	}
+	// TODO: is this allowed in C?
+	return ret;
+}
+
+as7265x_wavelengths_t as7265x_get_ordered_channel_wavelengths (void)
+{
+	int i = 0;
+	as7265x_wavelengths_t ret;
+	for (i = 0; i < 18; i++) {
+		ret.channel[i] = as7265x_unordered_channel_wavelength[i];
+	}
+	return ret;
+}
+
